7c0a54f0cbd838ee99d9a50fa2a74138736a22fe
Andrew Hearin <ahearin@anl.gov>
2022-02-17 16:56:47 -0600

    Add setup files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+PACKAGENAME = "SatGen"
+VERSION = "0.1.0"
+
+
+setup(
+    name=PACKAGENAME,
+    version=VERSION,
+    author="Fangzhou Jiang",
+    author_email="fzjiang@caltech.edu",
+    description="A semi-analytical satellite galaxy and dark matter halo generator",
+    long_description="A semi-analytical satellite galaxy and dark matter halo generator",
+    install_requires=["numpy", "scipy"],
+    packages=find_packages(),
+    url="https://github.com/aphearin/SatGen",
+)
